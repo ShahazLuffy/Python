a1fde6cf4a66b52b5da37cd160f0c4070cad6273
Shahaz <ali.ghaderipour@gmail.com>
2023-09-18 22:34:43 +0330

    140206282231

---
+++ modules/main.py
@@ -0,0 +1,11 @@
+import utility
+print(utility) # if u run this we have the directory of the utlity and we generate __pycache__
+# this __pycache__ will appear everytime we run a file with import a module
+print(utility.multiply(5,8))
+
+
+
+import shopping.shopping_cart
+print(shopping.shopping_cart)
+
+print(shopping.shopping_cart.buy('apple')) 
