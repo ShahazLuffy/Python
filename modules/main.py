db6e9765afdf1706e663c025cb2a378145d6bb0a
Shahaz <ali.ghaderipour@gmail.com>
2023-09-18 23:08:05 +0330

    140206282231

--- modules/main.py
+++ modules/main.py
@@ -2,10 +2,17 @@ import utility
 print(utility) # if u run this we have the directory of the utlity and we generate __pycache__
 # this __pycache__ will appear everytime we run a file with import a module
 print(utility.multiply(5,8))
+# another way of importing is like this
+from shopping.more_shopping.shopping_cart import buy
+print(buy('chicken'))
 
+import shopping.more_shopping.shopping_cart
+print(shopping.more_shopping.shopping_cart)
 
+print(shopping.more_shopping.shopping_cart.buy('apple'))
 
-import shopping.shopping_cart
-print(shopping.shopping_cart)
+from utility import multiply
+print(utility.multiply(2,55))
 
-print(shopping.shopping_cart.buy('apple')) 
+from shopping import shopping_cart
+print(shopping_cart.buy('zari'))
