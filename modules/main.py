7be817333961b1ebc6bc8a3a00530e354a2eda03
Shahaz <ali.ghaderipour@gmail.com>
2023-09-19 05:43:50 +0330

    140206290543

--- modules/main.py
+++ modules/main.py
@@ -1,4 +1,6 @@
+from utility import max
 import utility
+print(utility.multiply(2,55))
 print(utility) # if u run this we have the directory of the utlity and we generate __pycache__
 # this __pycache__ will appear everytime we run a file with import a module
 print(utility.multiply(5,8))
@@ -11,8 +13,9 @@ print(shopping.more_shopping.shopping_cart)
 
 print(shopping.more_shopping.shopping_cart.buy('apple'))
 
-from utility import multiply
-print(utility.multiply(2,55))
+
 
 from shopping import shopping_cart
 print(shopping_cart.buy('zari'))
+
+print(max([1,2,3,4]))
